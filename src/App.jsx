3eeb88f1c2981403df5f82e6bfafad9e1ea4e366
AlbiDota <alisaele1@gmail.com>
2025-01-22 15:01:17 +0100

    fidget spinner yay

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BildeFidgetToy from './components/BildeFidgetToy';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>Hello world</h1>;
+  <BildeFidgetToy/>
+
+  </div>
+  );
+}
+
+export default App;
