36f9f528b18476fb3f2a38b575edf48b8a3b632d
saraechk <sara-nada123@hotmail.com>
2025-01-28 16:42:07 +0100

    endra på navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/index.css";
+import pp from "../components/PP.png";
+import bell from "../components/bell.png";
 
 const Navbar = () => {
+  const handleSearch = () => {
+    const searchValue = document.querySelector(".searchbox input").value;
+    alert(`You searched: ${searchValue}`);
+  };
+
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/profile">My Profile</Link></li>
-        <li><Link to="/progress">Progress</Link></li>
-        <li><Link to="/friends">Friends</Link></li>
+        <li><Link to="/">HERCULES</Link></li>
+        <li><Link to="/Home">Home</Link></li>
+        <li><Link to="/Activity">Activity</Link></li>
+      </ul>
+      <div className="searchbox">
+        <input type="text" placeholder="Search..." />
+        <button className="searchbutton" onClick={handleSearch}>
+          Søk
+        </button>
+      </div>
+      <ul className="navbarbilde">
+        <li><img src={pp} alt="0" className="defaultpp" /></li>
+        <li><img src={bell} alt="0" className="defaultpp" /></li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
