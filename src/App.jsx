ee9d7199a45dff1ba486d22a1113ea7575baee0e
OscarGamst <56550067+OscarGamst@users.noreply.github.com>
2025-01-28 22:38:30 +0100

    Changes to routing so we can have a test 'branch' for components

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Progress from "./pages/Progress";
 import Activity from "./pages/Activity";
+import TestPageNoNav from "./pages/TestPageNoNav";
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Progress" element={<Progress />} />
           <Route path="/Activity" element={<Activity />} />
-        </Routes>
-      </MainLayout>
+        </Route>
+        <Route path="/TestPageNoNav" element={<TestPageNoNav />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/pages/TestPageNoNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ActivityFeed from "../components/ActivityFeed";
+
+
+const TestPageNoNav = () => {
+    return (
+      <div className="testpage">
+        <ActivityFeed />
+      </div>
+    );
+  };
+
+  export default TestPageNoNav;
