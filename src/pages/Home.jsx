be23ff6f93366573e05315b3bc0d9be24ea4f421
OscarGamst <oscar.gamst@hotmail.no>
2025-01-29 14:00:13 +0100

    CSS-flexbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 // src/pages/Dashboard.js
 import React from "react";
 import ActivityFeed from "../components/ActivityFeed";
+import SideBar from "../components/SideBar";
+import ProfileCard from "../components/ProfileCard";
 
 const Home = () => {
   return (
     <div className="home">
-      <ActivityFeed />
+      <div className="home-wrapper">
+        <div className="home-main">
+        <ActivityFeed />
+        </div>        
+        <div className="home-sidebar">
+          <ProfileCard />
+          </div>
+        
+      </div>
     </div>
   );
 };
