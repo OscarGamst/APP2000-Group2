d250902f7ca299ec7d0fc6868a6ebe3f83bf598f
OscarGamst <oscar.gamst@hotmail.no>
2025-01-30 15:38:52 +0100

    ja

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ActivityItem from "./ActivityItem";
 import "../styles/index.css";
 import activities from "./ActivityData";
 
-const ActivityFeed = () => {
+const ActivityFeed = () => { 
+  const [showFriends, setShowFriends] = useState(false); //sets showfriends to false (will show "myActivites")
+
+  const toggleFilter = () => {
+    setShowFriends(!showFriends); // changes the state (true/false)
+  };
+
   return (
     <div className="activity-feed">
       <h2>Activity Feed</h2>
-      {activities.map((activity) => (
-        <ActivityItem key={activity.id} activity={activity} />
-      ))}
+      <button id="filter-activity" onClick={toggleFilter}> {/*Triggers the filter function */}
+        {showFriends ? "Friends Activities" : "My Activities"} {/*button changes names based on state */}
+      </button>
+      {activities
+        .filter((activity) => (showFriends ? activity.username === false : activity.username === true))
+        .map((activity) => (
+          <ActivityItem key={activity.id} activity={activity} />
+        ))}
     </div>
   );
 };
 
 export default ActivityFeed;
+
+
+
 
 
