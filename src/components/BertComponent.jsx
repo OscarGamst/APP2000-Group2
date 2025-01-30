c9cf8752162df078f265b9e27bb8545c71270cc8
AlbiDota <alisaele1@gmail.com>
2025-01-30 15:48:48 +0100

    prototyping profile page at '/Albert'

--- src/components/BertComponent.jsx
+++ src/components/BertComponent.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "../styles/index.css";
+import default_pfp from "../pictures/default_pfp.png";
 
-const Activity = () => {
+const BertComponent = () => {
     return (
-        <div>
-            hehe
+        <div className="profileCard">
+            <div className="gradient"></div>
+            <ul>
+                <li></li>
+                <li><img src={default_pfp} alt=""/></li>
+                <li className="profileTitle">Username</li>
+                <li>
+                    <label>First name: </label>
+                    
+                </li>
+                <li>
+                    <label>Last name: </label>
+
+                </li>
+                <li>
+                    <label>E-mail: </label>
+
+                </li>
+                <li>
+                    <label>Birthday: </label>
+
+                </li>
+                <li>
+                    <label>Profile visibility: </label>
+
+                </li>
+            </ul>
         </div>
     );
 };
