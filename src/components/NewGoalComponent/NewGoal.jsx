6c41cd90277f9dd48eef90871fffcc66d5e17c12
saraechk <sara-nada123@hotmail.com>
2025-06-12 11:35:05 +0200

    fiksa på boksene til registrer activity, mangler knappene

--- src/components/NewGoalComponent/NewGoal.jsx
+++ src/components/NewGoalComponent/NewGoal.jsx
@@ -3,37 +3,32 @@ import "../../styles/index.css";
 import GoalRegistration from './GoalRegistration.jsx';
 
 const NewGoal = () => {
-    /*
-    Variable that determine the visibility of the buttons and the title - has to be
-    true by default, since we want to click the button to register a workout.
-    */
-    const[visibilityItems,setVisibilityItems]=useState(true);
-    const[visibilityGoalRegistration, setvisibilityGoalRegistration]=useState(false);
-
+    const [visibilityItems, setVisibilityItems] = useState(true);
+    const [visibilityGoalRegistration, setvisibilityGoalRegistration] = useState(false);
 
     const disableItems = () => {
         setVisibilityItems(false);
         setvisibilityGoalRegistration(true);
+    };
 
-    }
     const returnToDefault = () => {
         setVisibilityItems(true);
         setvisibilityGoalRegistration(false);
+    };
 
-    }
-
-    //Main code for component
     return (
         <div>
             {visibilityItems ? (
-                <div>
-                    <h4>Have a new goal in mind?</h4>
-                    <button onClick={disableItems}>Add Goal</button>
+                <div className="add-container">
+                    <h4 className="add-heading">Have a new goal in mind?</h4>
+                    <button onClick={disableItems} className="add-button">Add Goal</button>
                 </div>
             ) : null}
-            {visibilityGoalRegistration ? <GoalRegistration returnToDefault={returnToDefault}/> :null}
+            {visibilityGoalRegistration ? (
+                <GoalRegistration returnToDefault={returnToDefault} />
+            ) : null}
         </div>
     );
-}
+};
 
 export default NewGoal;
