a54dc5b8e7d1d055a829ecaa5394024899c68ed2
Malin Haugsdal <33.haugsdal@gmail.com>
2025-06-12 15:07:50 +0200

    added new update endpoint to activities

--- backend/workoutapp/src/main/java/org/example/workoutapp/repository/ActivityRepository.java
+++ backend/workoutapp/src/main/java/org/example/workoutapp/repository/ActivityRepository.java
@@ -3,6 +3,7 @@ package org.example.workoutapp.repository;
 import org.example.workoutapp.model.Activity;
 import org.example.workoutapp.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,18 @@ public interface ActivityRepository extends JpaRepository<Activity, Integer> {
             nativeQuery=true
     )
     List<Activity> findActivitiesTypeUsername(@Param("username") String username,  @Param("activityType") String activityType);
+
+    @Modifying
+    @Query(
+            value="UPDATE activity "+
+                    "SET title=:newTitle, description=:newDescription, duration=:newDuration, access=:newAccess "+
+                    "WHERE activity_id=:oldActivityId",
+            nativeQuery = true
+    )
+    void updateActivity(@Param("oldActivityId") Integer oldActivityId,
+                        @Param("newTitle") String newTitle,
+                        @Param("newDescription") String newDescription,
+                        @Param("newDuration") int newDuration,
+                        @Param("newAccess") String newAccess
+    );
 }
